f7c4af8465a7886363c15f6ea81622c022d27f0c
timonteutelink <timon@teutelink.nl>
2025-11-09 19:13:23 +0100

    very nice

--- devices/families/sdm845-mainline/sound.nix
+++ devices/families/sdm845-mainline/sound.nix
@@ -1,30 +1,30 @@
-{ config, lib, pkgs, ... }:
+{ config, lib, pkgs, ... }: let
+  sdm845-alsa-ucm = pkgs.callPackage (
+    { runCommand, fetchFromGitLab }:
 
-{
+    runCommand "sdm845-alsa-ucm" {
+      src = fetchFromGitLab {
+        name = "sdm845-alsa-ucm";
+        owner = "sdm845-mainline";
+        repo = "alsa-ucm-conf";
+        rev = "de81252f28465fb76e2aa58eb9733b88de2076ea"; # sdm845-phones
+        sha256 = "sha256-CgAPg0UUAJUE1gD59l2GNDx3h9crAato6O/dDJpRwiY=";
+      };
+    } ''
+      mkdir -p $out/share/
+      ln -s $src $out/share/alsa
+    ''
+  ) {};
+in {
   nixpkgs.overlays = [
     (self: super: {
-      sdm845-alsa-ucm = self.callPackage (
-        { runCommand, fetchFromGitLab }:
-
-        runCommand "sdm845-alsa-ucm" {
-          src = fetchFromGitLab {
-            name = "sdm845-alsa-ucm";
-            owner = "sdm845-mainline";
-            repo = "alsa-ucm-conf";
-            rev = "de81252f28465fb76e2aa58eb9733b88de2076ea"; # sdm845-phones
-            sha256 = "sha256-CgAPg0UUAJUE1gD59l2GNDx3h9crAato6O/dDJpRwiY=";
-          };
-        } ''
-          mkdir -p $out/share/
-          ln -s $src $out/share/alsa
-        ''
-      ) {};
+      inherit sdm845-alsa-ucm;
     })
   ];
 
   # Alsa UCM profiles
   mobile.quirks.audio.alsa-ucm-meld = true;
   environment.systemPackages = [
-    pkgs.sdm845-alsa-ucm
+    sdm845-alsa-ucm
   ];
 }
