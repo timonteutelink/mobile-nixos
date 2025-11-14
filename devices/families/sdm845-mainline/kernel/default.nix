35c429a8e8dc99e243d98bdbbf398783fa0d9808
timonteutelink <timon@teutelink.nl>
2025-11-14 05:19:36 +0100

    Update kernel

--- devices/families/sdm845-mainline/kernel/default.nix
+++ devices/families/sdm845-mainline/kernel/default.nix
@@ -4,14 +4,14 @@
 }:
 
 mobile-nixos.kernel-builder {
-  version = "6.11.0";
+  version = "6.16.7";
   configfile = ./config.aarch64;
 
   src = fetchFromGitLab {
     owner = "sdm845-mainline";
     repo = "linux";
-    rev = "sdm845-6.11.0";
-    hash = "sha256-Mf6lCozshFMSFHiy7AjS4dQ/jVWn0hXKFLcSHxwAwCc=";
+    rev = "sdm845-6.16.7-r0";
+    hash = "sha256-XYlXuzapuesiTpvquuz0b6yPyAqEdK9lMdglST+EZhk=";
   };
 
   isModular = false;
